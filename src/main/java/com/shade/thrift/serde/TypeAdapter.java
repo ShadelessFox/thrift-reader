6ab92c40af7543a5397260b451ffbd1bd194c843
ShadelessFox <yanuari.channel@gmail.com>
2024-09-12 17:30:45 +0200

    Remove test code

--- src/main/java/com/shade/thrift/serde/TypeAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.shade.thrift.serde;
-
-import com.shade.thrift.data.ThriftValue;
-
-public interface TypeAdapter<T> {
-    T deserialize(ThriftDeserializer deserializer, ThriftValue value);
-}
--- src/main/java/com/shade/thrift/serde/TypeAdapterFactory.java
+++
@@ -1,7 +0,0 @@
-package com.shade.thrift.serde;
-
-import java.lang.reflect.Type;
-
-public interface TypeAdapterFactory {
-    <T> TypeAdapter<T> create(Type type);
-}
